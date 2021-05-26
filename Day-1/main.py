5e0549f3267c1009c3d9f54991a68ec970c641b8
Jon <jon.heselton@gmail.com>
2021-05-26 07:39:50 -0500

    day1

---
+++ Day-1/main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Greetings, Generate a new brand name")
+#2. Ask the user for the city that they grew up in.
+city = input("What city were you born in? ")
+#3. Ask the user for the name of a pet.
+pet = input("What's the name of your Pet? ")
+#4. Combine the name of their city and pet and show them their band name.
+brand = pet + city
+print(ff"\n" + brand)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
