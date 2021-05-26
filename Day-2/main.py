76821476b8dcc741e4167e3df6957f178f43d650
Jon <jon.heselton@gmail.com>
2021-05-26 08:22:04 -0500

    day1

---
+++ Day-2/main.py
@@ -0,0 +1,10 @@
+#If the bill was $150.00, split between 5 people, with 12% tip.
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+#HINT 1: https://www.google.com/search?q=how+to+round+number+to+2+decimal+places+python&oq=how+to+round+number+to+2+decimal
+#HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
+bill_total = input("How much was the bill? ")
+int_bill_total = int(bill_total)
+per_person_total = "{:.2f}".format(round((int_bill_total * 1.12 / 5), 2))
+print("The per person total is "+ str(per_person_total))
