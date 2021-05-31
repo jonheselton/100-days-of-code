cdb0c35c6eb17f2fc67dda43abad6746bc1284f8
Jon <jon.heselton@gmail.com>
2021-05-31 12:35:59 -0500

    day 8 lesson 1 complete

---
+++ day8/main.py
@@ -0,0 +1,29 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(string,number):
+    # Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    # e.g.
+    # plain_text = "hello"
+    # shift = 5
+    # cipher_text = "mjqqt"
+    # print output: "The encoded text is mjqqt"
+    encrypted_string = ''
+    for letter in string:
+        encoded_letter = alphabet.index(letter) + number
+        if encoded_letter > 25:
+            encoded_letter += -25
+        encrypted_string += alphabet[encoded_letter]
+
+    return encrypted_string
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+# Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+print(encrypt(text, shift))
