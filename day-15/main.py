e2725fa8d61cc0f149687f6493ae2c8de8ced191
Jon <jon.heselton@gmail.com>
2021-06-04 16:05:56 -0500

    day 15

---
+++ day-15/main.py
@@ -0,0 +1,16 @@
+##!/usr/bin/python
+# create menu, order, power off, or report
+
+# check supplies
+# generate report (print supplies and cash)
+# insert coins
+# make change
+# compare supplies available with suupplies needed
+# make coffee
+def main():
+
+
+if __name__ == "__main__":
+    main()
+    
+    
