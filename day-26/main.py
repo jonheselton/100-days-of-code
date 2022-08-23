030a74fc3f067e0dd2821c9807c412d4fd36d0d3
jon <jon@jon.jon>
2022-08-23 16:16:49 -0500

    day 26

--- day-26/main.py
+++ day-26/main.py
@@ -1,14 +1,8 @@
-from tarfile import PAX_NAME_FIELDS
-
-
 import pandas
+
 nato_datafram = pandas.read_csv('nato_phonetic_alphabet.csv')
 input = input('enter a word')
 nato_dict = {row.letter:row.code for (index, row) in nato_datafram.iterrows()}
-
-
-result = {valuue for (key, valuue) in nato_dict.items() if key in input}
-print(result)
 
 result2 = [nato_dict[n] for n in input]
 print(result2)
